5faad9f78b0d818822603c2bca632a866a1d34ab
James Pan <jpanx2000@yahoo.com>
2022-11-28 18:17:40 -0700

    another commit

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,31 +1,31 @@
-import React from "react"
-import Header from "../common/header/Header"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Home from "../home/Home"
-import Footer from "../common/footer/Footer"
-import About from "../about/About"
-import Pricing from "../pricing/Pricing"
-import Blog from "../blog/Blog"
-import Services from "../services/Services"
-import Contact from "../contact/Contact"
+import React from "react";
+import Header from "../common/header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../home/Home";
+import Footer from "../common/footer/Footer";
+import About from "../about/About";
+import Pricing from "../pricing/Pricing";
+import Blog from "../blog/Blog";
+import Services from "../services/Services";
+import Contact from "../contact/Contact";
 
 const Pages = () => {
   return (
     <>
       <Router>
         <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/services' component={Services} />
-          <Route exact path='/blog' component={Blog} />
-          <Route exact path='/pricing' component={Pricing} />
-          <Route exact path='/contact' component={Contact} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/services" element={<Services />} />
+          <Route exact path="/blog" element={<Blog />} />
+          <Route exact path="/pricing" element={<Pricing />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
         <Footer />
       </Router>
     </>
-  )
-}
+  );
+};
 
-export default Pages
+export default Pages;
